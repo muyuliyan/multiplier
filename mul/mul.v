592754c043dfeec957bae143935eae3d0114f7b3
muyuliyan <1468256361@qq.com>
2025-04-06 22:53:26 +0800

    fix and remade the another multiplier

--- mul/mul.v
+++ mul/mul.v
@@ -11,47 +11,61 @@ module multiplier(
 
 reg x_31_buffer;
 reg y_31_buffer;
-reg [6:0]count;
+reg [7:0]count;
 reg [62:0] temp_x;
 reg [62:0] temp_y;
 reg [125:0]temp_r;
+
+wire complete_delay;
+wire real_complete;
+
+assign complete_delay = (count == 8'hf0);
+assign real_complete = (complete_delay || complete);
 
 always @(posedge mul_clk) begin 
     if(reset) begin
         x_31_buffer <= 1'b0;
         y_31_buffer <= 1'b0;
-        count <= 7'b0;
     end
     else begin
         x_31_buffer <= x[31];
         y_31_buffer <= y[31];
-        count <= 1'b0;
     end
 end
 
 always @(posedge mul_clk) begin
-    case({x_31_buffer, y_31_buffer})
+    if(reset || ~mul)begin
+        count <= 8'd63;
+        temp_r <= 126'b0;
+    end
+    else begin
+       case({x_31_buffer, y_31_buffer})
         2'b00: temp_x <= {31'b0, x[31:0]},temp_y <= {31'b0, y[31:0]};
         2'b01: temp_x <= {31'b0, x[31:0]},temp_y <= {31'b1, y[31:0]};
         2'b10: temp_x <= {31'b1, x[31:0]},temp_y <= {31'b0, y[31:0]};
         2'b11: temp_x <= {31'b1, x[31:0]},temp_y <= {31'b1, y[31:0]};
         default: temp_x <= 1'bx,temp_y <= 1'bx;
-    endcase
+    endcase 
+    end
+    else begin
+        count   <= 8'hf0;
+    end
+
 end
 
 always @(posedge mul_clk) begin
     if(temp_y[0]){
         temp_r <= temp_r + temp_x << count;
-        count <= count + 1;
+        count <= count + 8'd1;
         temp_y <= temp_y >> 1;
     }
     else{
-        count <= count + 1;
+        count <= count - 8'd1;
         temp_y <= temp_y >> 1;
     }
 end
 
 assign result = temp_r[62:0];
-assign complete = (count == 7'd63);
+assign complete = (count == 8'd0);
 
 endmodule
--- mul/mult_gen_1.v
+++ mul/mult_gen_1.v
@@ -5,36 +5,6 @@ module mult_gen_1(
     input mul,
     input [31:0] A,
     input [31:0] B,
-    output [31:0] P,
+    output [63:0] P,
     input SCLR,
 )
-
-reg  mul_unsigned_buffer; 
-reg  a_31_buffer;
-reg  b_31_buffer;
-integer i;
-
-always @(posedge CLK) begin 
-    if (SCLR) begin 
-        mul_unsigned_buffer <= 1'b0;
-        a_31_buffer <= 1'b0;
-        b_31_buffer <= 1'b0;
-    end 
-    else if (mul) begin 
-        mul_unsigned_buffer <= mul_unsigned;   
-        a_31_buffer <= A[31];
-        b_31_buffer <= B[31];
-    end
-end
-
-    always @(posedge CLK)begin
-        P <= 32'b0;
-        for( i = 0; i < 32; i = i + 1) begin
-            if(b_31_buffer[0] == 1) begin
-                P = P + (a_31_buffer << i);
-            end
-
-            b_31_buffer = b_31_buffer >> 1;
-        end
-    end
-endmodule
